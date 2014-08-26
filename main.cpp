558987ce53a4945b9f62ea9b6d70e8b53e369408
Fernando Jorge Mota <f.j.mota13@gmail.com>
2014-08-26 00:00:30 -0300

    Adiciona exemplo de uso da fila

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <cstdio>
 #include "Lista.hpp"
+#include "Fila.cpp"
 
 using std::printf;
 
@@ -27,6 +28,23 @@ int main(int argc, char** argv) {
     printf("O tamanho da lista é: %d\n", teste.pegaTamanho());
     teste.troca(1, 3);
     printf("O segundo valor é: %d\n", teste.pegaValor(1));
-    printf("O quarto valor é: %d", teste.pegaValor(3));
+    printf("O quarto valor é: %d\n", teste.pegaValor(3));
+    
+    Fila testeFila = Fila(10);
+    printf(testeFila.filaCheia()? "A fila esta cheia\n": "A fila nao esta cheia\n");
+    printf(testeFila.filaVazia()? "A fila esta vazia\n": "A fila nao esta vazia\n");
+    for(int a=0; a < 10; a++) {
+        printf("Adicionando numero %d na fila\n", a);
+        testeFila.inclui(a);
+    }
+    printf(testeFila.filaCheia()? "A fila esta cheia\n": "A fila nao esta cheia\n");
+    printf(testeFila.filaVazia()? "A fila esta vazia\n": "A fila nao esta vazia\n");
+    for(int a=0; a < 10; a++) {
+        printf("Retirando numero da fila: O numero é %d\n", testeFila.ultimo());
+        testeFila.retira();
+    }
+    printf(testeFila.filaCheia()? "A fila esta cheia\n": "A fila nao esta cheia\n");
+    printf(testeFila.filaVazia()? "A fila esta vazia\n": "A fila nao esta vazia\n");
+    
     return EXIT_SUCCESS;
 }
