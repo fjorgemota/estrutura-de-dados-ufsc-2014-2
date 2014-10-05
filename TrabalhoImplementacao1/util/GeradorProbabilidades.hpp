2fa5de99e9b32b683c0a0cacf9ec507e9d3117b9
Fernando Jorge Mota <f.j.mota13@gmail.com>
2014-10-05 13:51:52 -0300

    Adicionado gerador de probabilidade para facilitar na criação de listas que completem 100% de elementos e ajudem na randomização de um determinado item

---
+++ TrabalhoImplementacao1/util/GeradorProbabilidades.hpp
@@ -0,0 +1,16 @@
+// Copyright 2014 Caique Rodrigues Marques e Fernando Jorge Mota
+
+#ifndef GERADOR_PROBABILIDADES_HPP
+#define GERADOR_PROBABILIDADES_HPP
+#include "ListaDupla.cpp"
+
+template <typename T>
+class GeradorProbabilidades {
+ private:
+ 	ListaDupla<T> *lista;
+ public:
+	GeradorProbabilidades();
+	void adiciona(T dado, int porcentagem);
+	ListaDupla<T> pegaLista();
+};
+#endif /* GERADOR_PROBABILIDADES_HPP */
