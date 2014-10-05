5ee32fc27ee7a83dc67efd2acaddf2195ce3d588
CaiqueMarques <caiquermarques95@gmail.com>
2014-10-05 23:40:07 +0000

    Classe Pista com novos metodos e atributos incluidos

--- TrabalhoImplementacao1/pista/Pista.cpp
+++ TrabalhoImplementacao1/pista/Pista.cpp
@@ -5,10 +5,11 @@
 #include "Pista.hpp"
 #include "../util/FilaEnc.cpp"
 
-Pista::Pista(int tamanho, int velocidade) : FilaEnc() {
+Pista::Pista(int tamanho, int velocidade, ListaDupla<T> pistas) : FilaEnc() {
     this->tamanho = tamanho;
     this->velocidade = velocidade;
     this->tamanhoDisponivel = velocidade;
+    this->pistasSaida = pistas;
 }
 
 int Pista::pegaTamanho() {
@@ -20,14 +21,38 @@ int Pista::pegaVelocidade() {
 }
 
 bool Pista::adiciona(Carro* carro) {
-	int temporario = this->tamanhoDisponivel - car->pegaTamanho();
-	if (temporario > 0) {
-		this->inclui(carro);
-		this->tamanhoDisponivel = temporario;
-		return true;
-	} else {
-		return false;
-	}
+    if (this->estaCheia(carro)) {
+        return false;
+    }
+    int tempo = this->tamanho / this->velocidade;
+    this->inclui(carro);
+    this->tamanhoDisponivel -= carro->pegaTamanho();
+    return true;
+}
+
+Carro Pista::sairDaPista() {
+    Carro* carro = this->retira();
+    this->tamanhoDisponivel += carro->pegaTamanho();
+    return carro;
+}
+
+void Pista::transfere() {
+    Carro* ultimo = this->ultimo();
+    Pista destino = this->sorteiaPista();
+    if (destino->estaCheia(ultimo)) {
+        ultimo = this->sairDaPista();
+        destino->adiciona(ultimo);
+    }
+}
+
+Pista Pista::sorteiaPista() {
+    int sorteio = rand() % 100;
+    return this->pistasSaida->mostra(sorteio);
+}
+
+bool Pista::estaCheia(Carro* carro) {
+    int temporario = this->tamanhoDisponivel - carro->pegaTamanho();
+    return temporario >= 0;
 }
 
 #endif /* PISTA_CPP */
--- TrabalhoImplementacao1/pista/Pista.hpp
+++ TrabalhoImplementacao1/pista/Pista.hpp
@@ -4,6 +4,7 @@
 #define PISTA_HPP
 #include "../util/FilaEnc.cpp"
 #include "../carro/Carro.cpp"
+#include "ListaDupla.hpp"
 
 class Pista: private FilaEnc<Carro*> {
  private:
@@ -11,12 +12,18 @@ class Pista: private FilaEnc<Carro*> {
     int velocidade;  //!< A velocidade limite que a pista possui, os carros andarao nesta velocidade
     int tamanhoDisponivel;   //!< O tamanho que esta disponivel, ou seja, 
     						// definira se ela suportara mais carros
+    ListaDupla<Pista> pistasSaida;
+    FilaEnc<Carro*> carrosbloqueados;
 
  public:
-    Pista(int tamanhoMaximo, int velocidade);
+    Pista(int tamanhoMaximo, int velocidade, ListaDupla<T> pistas);
     int pegaTamanhoMaximo();
     int pegaVelocidade();
     bool adiciona(Carro* carro);
+    Carro sairDaPista();
+    void transfere();
+    Pista sorteiaPista();
+    bool estaCheia(Carro* carro);
 };
 
 #endif /* PISTA_HPP */
