18031e37b50b128b9e141443938f5a761e412f6f
CaiqueMarques <caiquermarques95@gmail.com>
2014-11-26 00:27:33 -0200

    Classe Indexador documentada

--- TrabalhoImplementacao2/programa/Indexador.hpp
+++ TrabalhoImplementacao2/programa/Indexador.hpp
@@ -12,24 +12,62 @@
  */
 class Indexador {
  private:
- 	IndicePrimario *indicePrimario;
+ 	IndicePrimario *indicePrimario; 
+ 		//!< Arvore de indice primario que armazenara
+ 		//!< os comandos referenciados pelos arquivos 
  	IndiceSecundario *indiceSecundario;
+ 		//!< Arvore de indice secundario que armazenara
+ 		//!< as palavras referenciadas pelos arquivos
  	RemovedorLexico *removedor;
+ 		//!< Conectivos que serao ignorados na indexaçao
 
+ 	/*!
+ 	 * Metodo que remove a extensao '.txt' do
+ 	 * arquivo manual page
+ 	 *
+ 	 * @param nome O nome do arquivo que sera verificado
+ 	 */
  	string* retiraExtensao(char *nome);
 
+ 	/*!
+ 	 * Metodo que le os conteudos do arquivo 
+ 	 * manual page referenciado
+ 	 *
+ 	 * @param nome O nome do arquivo que sera lido
+ 	 */
  	string* leConteudo(char *nome);
 
  public:
+ 	/*!
+ 	 * Controi um indexador
+ 	 *
+ 	 * @param removedor A lista de conectivos ignorados
+ 	 * para a indexaçao
+ 	 */
  	explicit Indexador(RemovedorLexico *removedor);
 
+ 	/*!
+ 	 * Indexa (identifica) um arquivo
+ 	 *
+ 	 * @param nome O nome do arquivo que sera indexado
+ 	 */
  	void indexaArquivo(char *nome);
 
+ 	/*!
+ 	 * Metodo que gera e salva os arquivos na arvore
+ 	 * de indice primario
+ 	 *
+ 	 * @param nome O nome do arquivo a ser salvo
+ 	 */
  	void salvaIndicePrimario(char *nome);
 
+ 	/*!
+ 	 * Metodo que gera e salva os arquivos na arvore
+ 	 * de indice secundario
+ 	 *
+ 	 * @param nome O nome do arquivo a ser salvo
+ 	 */
  	void salvaIndiceSecundario(char *nome);
-
- 	void executa();
 };
 
 #include "Indexador.cpp"
